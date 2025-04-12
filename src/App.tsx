9f3beb71533664503652530383ec11c1b9eb973f
paulof9 <above2@outlook.com>
2025-04-12 16:03:50 -0300

    configura rotas com react-router e integra página inicial

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import ClienteDetalhes from './pages/ClienteDetalhes';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cliente/:cpfCnpj" element={<ClienteDetalhes />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
